24c4926c734230ba860453be7b623fc1078e9dff
maggie-njeri <mkiri00@gmail.com>
2024-04-20 12:15:34 -0700

    flask

--- models/amenity.py
+++ models/amenity.py
@@ -5,7 +5,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.place import place_amenity
 
+
 class Amenity(BaseModel):
-     __tablename__ = "amenities"
+    __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -17,9 +17,10 @@ place_amenity = Table("place_amenity", Base.metadata,
                              primary_key=True,
                              nullable=False))
 
+
 class Place(BaseModel):
     """ A place to stay """
-       __tablename__ = "places"
+    __tablename__ = "places"
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
