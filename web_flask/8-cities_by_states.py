6c70071e3ab57b4627a2763974d0a172b992eb72
maggie-njeri <mkiri00@gmail.com>
2024-04-21 12:20:52 -0700

    flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -30,5 +30,6 @@ def remove_SQLalc_session(exception):
     """ close storage when tear down is called """
     storage.close()
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
