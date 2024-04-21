7f5eef9f770504566f4d7a7c1783e0f75a2087c4
maggie-njeri <mkiri00@gmail.com>
2024-04-21 10:53:21 -0700

    flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-"""
-    this script starts a Flask web application
-"""
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes: - /states_list """
+from models import *
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from flask import Flask, render_template
-from models import storage
-import os
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def handle_teardown(self):
-    """
-        method to handle teardown
-    """
-    storage.close()
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def city_state_list():
-    """
-        method to render states from storage
-    """
-    states = storage.all('State').values()
-    return render_template("8-cities_by_states.html", states=states)
+def cities_by_states():
+    """ display HTML page with list of cities """
+    states = storage.all(classes["State"]).values()
+    # ^ fetches states data from storage engine, then in line below,
+    # those states are passed into the template
+    return render_template('8-cities_by_states.html', states=states)
 
+
+@app.teardown_appcontext
+def remove_SQLalc_session(exception):
+    """ close storage when tear down is called """
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
